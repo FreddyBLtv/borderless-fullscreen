54a261b8c1e75d175595bbbda3bb72bc74c2a5b4
Freddy Ugwu <93346974+FreddyBLtv@users.noreply.github.com>
2023-02-06 13:08:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-cp borderless-fullscreen.sh /usr/bin 
+read -p "Are you running on Wayland (y/n)? " wayland
+
+if [ "$wayland" == "y" ]; then
+  cp borderless-fullscreen-wayland.sh /usr/bin
+  cp borderless-fullscreen-wayland.desktop /usr/share/applications/
+  chmod +x /usr/bin/borderless-fullscreen-wayland.sh
+  chmod +x /usr/share/applications/borderless-fullscreen-wayland.desktop
+else
+  read -p "Are you running on Xorg (y/n)? " xorg
+  if [ "$xorg" == "y" ]; then
+    cp borderless-fullscreen.sh /usr/bin
+    cp borderless-fullscreen.desktop /usr/share/applications/
+    chmod +x /usr/bin/borderless-fullscreen.sh
+    chmod +x /usr/share/applications/borderless-fullscreen.desktop
+  fi
+fi
+
 cp icon-bf.png /usr/share/icons/
-cp borderless-fullscreen.desktop /usr/share/applications/ 
-chmod +x /usr/bin/borderless-fullscreen.sh
-chmod +x /usr/share/applications/borderless-fullscreen.desktop
 chmod +x uninstall.sh
+
+echo "Installed Successfully!"
