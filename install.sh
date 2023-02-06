b55209f4eeabf290711d005e1c1313c0c82a2a90
Freddy Ugwu <93346974+FreddyBLtv@users.noreply.github.com>
2023-02-05 21:28:50 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 cp borderless-fullscreen.sh /usr/bin 
 cp icon-bf.png /usr/share/icons/
 cp borderless-fullscreen.desktop /usr/share/applications/ 
-cp borderless-fullscreen.desktop $HOME/.local/share/applications
 chmod +x /usr/bin/borderless-fullscreen.sh
 chmod +x /usr/share/applications/borderless-fullscreen.desktop
 chmod +x uninstall.sh
