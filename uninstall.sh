f3decead96cfbbe60319ca4c009e46be4b1d1e2c
Freddy Ugwu <93346974+FreddyBLtv@users.noreply.github.com>
2023-02-05 21:22:24 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,3 +3,4 @@
 rm /usr/bin/borderless-fullscreen.sh
 rm /usr/share/icons/icon-bf.png
 rm /usr/share/applications/borderless-fullscreen.desktop
+rm $HOME/.local/share/applications/borderless-fullscreen.desktop
