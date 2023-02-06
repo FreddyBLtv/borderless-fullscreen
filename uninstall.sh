c90aa7cc62d57863c7bea95693a71d890a38d485
Freddy Ugwu <93346974+FreddyBLtv@users.noreply.github.com>
2023-02-06 13:09:11 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-rm /usr/bin/borderless-fullscreen.sh
-rm /usr/share/icons/icon-bf.png
-rm /usr/share/applications/borderless-fullscreen.desktop
-cd ..
-rm -rf borderless-fullscreen
+if [ -e "/usr/bin/borderless-fullscreen-wayland.sh" ]; then
+  rm /usr/bin/borderless-fullscreen-wayland.sh
+fi
+
+if [ -e "/usr/bin/borderless-fullscreen.sh" ]; then
+  rm /usr/bin/borderless-fullscreen.sh
+fi
+
+if [ -e "/usr/share/icons/icon-bf.png" ]; then
+  rm /usr/share/icons/icon-bf.png
+fi
+
+if [ -e "/usr/share/applications/borderless-fullscreen.desktop" ]; then
+  rm /usr/share/applications/borderless-fullscreen.desktop
+fi
+
+if [ -e "/usr/share/applications/borderless-fullscreen-wayland.desktop" ]; then
+  rm /usr/share/applications/borderless-fullscreen-wayland.desktop
+fi
+
+echo "Uninstalled Successfully!"
